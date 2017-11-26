092a1e1a5f43c20114630ff028744560ec32f40d
ivarledgis <ivarledgis90@gmail.com>
2017-11-26 21:59:18 +0545

    firts website upload

---
+++ about.php
@@ -0,0 +1,18 @@
+<div id="about">
+			
+			
+			<?php include('mycv.php');?>
+
+
+
+
+		
+	
+		
+	</div>
+<!-- <section class="section section-dark" id="2">
+	
+	<h1>Resume</h1>
+	<p> lorem100	lorem100	lorem100	lorem100	lorem100	lorem100	lorem100				
+	</p>	
+</section> -->
---
+++ contact.php
@@ -0,0 +1,12 @@
+<section class="section section-dark" id="3">
+	
+	<h1>Contact</h1>
+</section>	
+<div id="contact">
+		
+			
+			<?php include('form.php') ;?>
+
+		
+		
+	</div>
